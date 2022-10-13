fbbe84666bfd9b5c5c8794ca15f879ba0afc2c6d
mathusan selvakumar <mathusan.selvakumar@gmail.com>
2022-10-13 12:32:36 +0200

    exercice 2 du tp1 resolu

--- tp/tp1/Exercice2.java
+++ tp/tp1/Exercice2.java
@@ -1,52 +1,54 @@
 public class Exercice2 {
-    private static void affiche(int[] t) {
+    private void affiche(int[] t) {                         // Affiche un tableau d'entiers t
         System.out.print("[");
-        
-        for (int i = 0; i < t.length - 1; i++) {
-            System.out.print(t[i]);
-            System.out.print(", ");
+        for (int i = 0; i < t.length; i++) {
+            if (i == t.length - 1) {                        // le cas du dernier élément
+                System.out.print(t[i]);
+            } else {
+                System.out.print(t[i]);
+                System.out.print(", ");
+            }
         }
-
-        if (t.length > 0) {
-            System.out.print(t[t.length - 1]);
-        }
-
-        System.out.print("]");
+        System.out.println("]");
     }
 
-    private static int[] multiplication(int[] t1, int[] t2) {
-        int[] res = new int[Math.max(t1.length, t2.length)];
-
+    private int[] multiplication(int[] t1, int[] t2) {              // retourne un tableau de taille max(len(t1), len(t2)) obtenu
+        int[] res = new int[Math.max(t1.length, t2.length)];        // en multipliant t1 et t2 case par case
         for (int i = 0; i < res.length; i++) {
             int t1n = i <= t1.length - 1 ? t1[i] : 1;
             int t2n = i <= t2.length - 1 ? t2[i] : 1;
             res[i] = t1n * t2n;
         }
-
         return res;
     }
 
-    private static int[] split(int[] t) {
-        String tmp = "";
+    private int[] split(int[] t) {                          // retourne le tableau obtenu en séparant chacun des chiffres
+        String tmp = "";                                    // des entiers du tableau de départ
         for (int i = 0; i < t.length; i++) {
             tmp += String.valueOf(t[i]);
         }
-
         int[] res = new int[tmp.length()];
         for (int i = 0; i < tmp.length(); i++) {
             res[i] = Character.getNumericValue(tmp.charAt(i));
         }
-
         return res;
     }
 
     public static void main(String[] args) {
-        int[] t = {1, 2, 3, 4};
-        affiche(t);
-        
-        int[] s = {11, 21, 41};
-        affiche(multiplication(t, s));
+        Exercice2 e2 = new Exercice2();
 
-        affiche(split(s));
+        int[] t = {1, 3, 6, 7};
+        int[] s = {2, 4, 6};
+        e2.affiche(t);
+        System.out.println("*");
+        e2.affiche(s);
+        System.out.println("=");
+        e2.affiche(e2.multiplication(t, s));
+
+        System.out.println();
+        t = new int[]{2, 12, 36, 7};
+        e2.affiche(t);
+        System.out.println("Split chiffres : ");
+        e2.affiche(e2.split(t));
     }
 }
--- tp/tp1/README.md
+++ tp/tp1/README.md
@@ -5,3 +5,9 @@ D'après la documentation `Java` :
 - le type de sortie de la méthode `main` est `void` (ce dernier ne retourne rien) ;
 - la méthode `length` dans `String` permet de calculer la taille d’une chaîne de caractères ;
 - deux méthodes `hypot` existent : une dans la classe `Math` et l'autre dans la classe `StrictMath`.
+
+## [Exercice 2](Exercice2.java)
+#### `String.valueOf(n)`
+Retourne la représentation en chaîne de caractères à partir d'un entier.
+#### `Character.getNumericValue(c)`
+Retourne l'entier spécifié par le caractère considéré.
