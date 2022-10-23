3bb63b4d77f2bca56b2924d336a6f1f7ca3c0d04
mathusanMe <89030828+mathusanMe@users.noreply.github.com>
2022-10-23 19:51:22 +0200

    Delete ChaineCar.java

--- tp/tp5/ChaineCar.java
+++
@@ -1,8 +0,0 @@
-public abstract class ChaineCar {                   // Classe abstraite : peut etre heritee mais on ne peut pas creer d'instance de ChaineCar
-    
-    public abstract int len();                      // Fonction abstraite donc rien dans le corps
-
-    public String toString() {                      // renvoie la chaine vide
-        return "";  
-    }
-}
